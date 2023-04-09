7cda8786c8d856f7bce8257c3a7048f3e9876b6b
Nikita <86921169+MasterNikPro@users.noreply.github.com>
2023-04-09 10:47:24 +0300

    feat: Created layout for login fragment Added layout for email and password input fields, as well as text links for creating a new account and password recovery

--- app/src/main/java/com/nikzakharenko/crazytictactoe/MainActivity.kt
+++ app/src/main/java/com/nikzakharenko/crazytictactoe/MainActivity.kt
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-    var lol:Int=10;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
